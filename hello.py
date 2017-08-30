be3baac6b182f9a2d2ecbff63438aca2ba7cf20b
Mabs20 <31477799+Mabs20@users.noreply.github.com>
2017-08-30 09:14:53 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+# This program says hello and greets a person by name
+#
+# Marc
+# August 24, 2017
+
+print(" Hello. ")
+print(" What is your name? ")
+name = input()
+print(" It is good to meet you, " + name + " . ")
+print(" Where were you born, " + name + " ? ")
+born = input()
+print(" That's interesting. I'd like to visit " + born + " one day. ")
---
+++ madlib.py
@@ -0,0 +1,7 @@
+noun1 = input (" Tell me a noun ")
+noun2 = input (" Tell me another noun ")
+verb1 = input (" Now, tell me a verb ")
+verb2 = input (" Another verb ")
+adjective1 = input (" Now, an adjective ")
+place = input (" Think of a place ")
+print(" 
